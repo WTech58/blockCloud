e53edf39348daa54b5095c68ff82d808b1b7f292
WTech58 <zc949466@gmail.com>
2024-11-30 07:28:16 +0800

    Update __init__.py

--- src/install/__init__.py
+++ src/install/__init__.py
@@ -1,4 +1,5 @@
 from ..app import client
+from time import sleep
 
 def images_exists(imageName):
   images = client.images.list()
@@ -11,15 +12,18 @@ def install_image(target):
   if target == "basic":
     if not images_exists("bcBasic"):
       client.images.build(path=".", tag="bcBasic")
+      sleep(30)
       return "bcBasic"
     return "bcBasic"
   elif target == "normal":
     if not images_exists("bcNormal"):
       client.images.build(path=".", tag="bcNormal")
+      sleep(30)
       return "bcNormal"
     return "bcNormal"
   elif target == "pro":
     if not images_exists("bcPro"):
       client.images.build(path=".", tag="bcPro")
+      sleep(30)
       return "bcPro"
     return "bcPro"
