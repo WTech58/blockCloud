04a66003fb202ca251cae38771697ab82235c0cb
WTech58 <zc949466@gmail.com>
2024-12-01 11:13:09 +0800

    Update router.py

--- src/app/router.py
+++ src/app/router.py
@@ -1,5 +1,6 @@
 from . import client,app
-from flask import render_template
+from ..install import install_image,install_mach
+from flask import render_template,request
 
 @app.route("/")
 def index():
@@ -12,3 +13,13 @@ def dash():
 @app.route("/shop")
 def shop():
   return render_template("shop.html")
+
+@app.route("/start")
+def start_mach():
+  target = request.args.get("target")
+  if not taget or target == "" or target == None:
+    return "找不到目標"
+  else:
+    image = install_image(target)
+    vm = install_mach(image)
+    return render_template("dash.html",image=image,vm=vm)
