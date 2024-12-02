d2ec4ce0f4c9dd99074a8c8df30256aefe80899f
WTech58 <zc949466@gmail.com>
2024-12-02 21:41:43 +0800

    Update router.py

--- src/app/router.py
+++ src/app/router.py
@@ -1,6 +1,7 @@
 from . import client,app
 from ..install import install_image,install_mach
 from flask import render_template,request
+from hashlib import sha256
 
 @app.route("/")
 def index():
@@ -10,6 +11,8 @@ def index():
 def dash():
   return render_template("dash.html")
 
-
+@app.route("/upload",methods=["GET","POST"])
+def upload():
+  pass
 
 app.run(host="0.0.0.0",port=5000)
