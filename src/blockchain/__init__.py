b581f5c478c309f3cf34cd0d23a176751ba3d333
WTech58 <zc949466@gmail.com>
2024-12-03 19:23:44 +0800

    Update __init__.py

--- src/blockchain/__init__.py
+++ src/blockchain/__init__.py
@@ -2,9 +2,13 @@ from hashlib import sha256
 from base64 import b64encode,b64decode
 
 class BlockSave:
-  def __init__(self,imageData:str=None,textData:str=None):
+  def __init__(self,imageData:str=None):
     self.imageData = imageData
-    self.textData = textData
+    self.imageTitle = ""
     self.bimage = ""
     if self.imageData != None or self.imageData != "":
       self.bimage = b64encode(self.imageData.encode())
+  def _upload(self,image:str=None):
+    pass
+  def _get(self,imageText):
+    pass
