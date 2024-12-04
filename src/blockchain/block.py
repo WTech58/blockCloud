752a7db9ff22d98c85641e48cf9088a352ba0b71
WTech58 <zc949466@gmail.com>
2024-12-04 10:17:10 +0800

    Build block

--- src/blockchain/block.py
+++ src/blockchain/block.py
@@ -2,9 +2,10 @@ from hashlib import sha256
 from base64 import b64encode,b64decode
 
 class Block:
-  def __init__():
-    pass
-
+  def __init__(self,blockID:int,data,prevData:str=None):
+    self.blockID = blockID
+    self.data = data
+    self.prevData = prevData
 class BlockChain(Block):
   def __init__():
     pass
