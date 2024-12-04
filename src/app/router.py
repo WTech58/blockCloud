7479e6370463cb5ba60de267eb084666f7ff721d
WTech58 <zc949466@gmail.com>
2024-12-05 06:49:26 +0800

    debug: change Flask object

--- src/app/router.py
+++ src/app/router.py
@@ -1,5 +1,5 @@
 from blockchain import BlockSave
-from flask import render_template,request,abort,session
+from flask import Flask,render_template,request,abort,session
 from hashlib import sha256
 import os
 
